3bc1bf623ff1e8bf8b3ec2617a5668fa55840792
maxime <maxime.moutet@etu.univ-amu.fr>
2021-03-03 12:10:08 +0100

    first commit stage b

---
+++ Stage_B.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy.linalg.interpolative
+
+
+def direct_svd(q, a):
+    """
+        param q: orthonormal matrix whose range captures the
+                action of the input matrix a
+        
+        param a: input matrix
+
+        returns: an array with the svd of a
+    """
+
+    b = q.T.dot(a)
+    ub, s, vh = np.linalg.svd(b, full_matrices=False)
+    s = np.diag(s)
+
+    return np.array([q.dot(ub), s, vh])
+
+
+def svd_row_extraction(q, a, l):
+    idx, x = scipy.linalg.interpolative.interp_decomp(q.T, l)
+    print(q)
+    print(np.dot(x, q.T[idx, :]))
+
+    # aj = a[idx, :]
+    # rh, wh = np.linalg.qr(aj)[0, 1]
+
+    # z = np.dot(proj, rh)
+
+
+a = np.array([[1, 1, 1], [1, 1, 0], [1, 0, 0]])
+q = randomized_range_finder(a, 2)
+
+svd_row_extraction(q, a, 2)
