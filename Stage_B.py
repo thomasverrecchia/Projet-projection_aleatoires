b1d5ef6ac1aa5b270409e515f038ba4ba0b26da0
maxime <maxime.moutet@etu.univ-amu.fr>
2021-03-17 21:18:54 +0100

    correction direct svd

--- Stage_B.py
+++ Stage_B.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy.linalg.interpolative
+
+
 from Stage_A import*
 
 
@@ -14,16 +16,20 @@ def direct_svd(q, a):
     """
 
     b = q.T.dot(a)
-    ub, s, vh = np.linalg.svd(b, full_matrices=False)
-    s = np.diag(s)
+    ub, S, vh = np.linalg.svd(b, full_matrices=True)
+    m = ub.shape[1]
+    n = vh.shape[0]
+    s = np.zeros((m, n))
+    for i in range(len(S)):
+        s[i, i] = S[i]
 
     return np.array([q.dot(ub), s, vh])
 
 
-def svd_row_extraction(q, a, l):
-    idx, x = scipy.linalg.interpolative.interp_decomp(q.T, l)
-    print(q)
-    print(np.dot(x, q.T[idx, :]))
+# def svd_row_extraction(q, a, l):
+#     idx, x = scipy.linalg.interpolative.interp_decomp(q.T, l)
+#     print(q)
+#     print(np.dot(x, q.T[idx, :]))
 
     # aj = a[idx, :]
     # rh, wh = np.linalg.qr(aj)[0, 1]
@@ -31,3 +37,6 @@ def svd_row_extraction(q, a, l):
     # z = np.dot(proj, rh)
 
 
+a = np.array([[1, 1, 1, 1], [2, 3, 4, 5], [6, 1, 0, 0]])
+q = randomized_range_finder(a, 4)
+print(np.linalg.multi_dot(direct_svd(q, a)))
